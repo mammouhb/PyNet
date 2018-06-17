ad02caed986fe34eff1970e7c2bae4ef5e08be77
Applied Python user <mammouhb@gmail.com>
2018-06-17 15:04:52 -0700

    read and write yaml format

--- lesson1.py
+++ lesson1.py
@@ -1,3 +1,4 @@
+import pprint
 import yaml
 
 my_list = []
@@ -8,4 +9,16 @@ print(my_list[0]['device'])
 yaml_my_list = yaml.dump(my_list, default_flow_style=True)
 
 print(yaml_my_list)
+
+yaml_my_list = yaml.dump(my_list, default_flow_style=False)
+
+print(yaml_my_list)
+
+with open("some_file.yml", "w") as f:
+  f.write(yaml.dump(my_list, default_flow_style=False))
+
+with open("yml_file.yml") as f:
+  new_list = yaml.load(f)
+  print(new_list)
+  pprint.pprint(new_list)
 
