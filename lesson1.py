5a3c7286d368a4b752a55501e2be5f1c418e9d83
Applied Python user <mammouhb@gmail.com>
2018-06-19 19:02:43 -0700

    adding ciscoconfparse

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,16 @@
 import pprint
 import yaml
+import json
+from ciscoconfparse import CiscoConfParse
 
 my_list = []
 my_list = [{'device':'juniper','IP_addr':'192.168.2.1'},{'IP_addr':'192.168.1.1','device':'cisco'},range(2)]
 print(my_list)
 print(my_list[0]['device'])
+
+print ("***********************************************")
+print("********************YAML**********************")
+print ("***********************************************")
 
 yaml_my_list = yaml.dump(my_list, default_flow_style=True)
 
@@ -21,4 +27,43 @@ with open("yml_file.yml") as f:
   new_list = yaml.load(f)
   print(new_list)
   pprint.pprint(new_list)
+
+print ("***********************************************")
+print ("************************JSON**************************")
+print ("***********************************************")
+
+json_my_list = json.dumps(my_list)
+
+print(json_my_list)
+
+with open("json_file.json", "w") as f:
+  json.dump(my_list, f)
+
+with open("json_file.json") as f:
+  json_new_list = json.load(f)
+pprint.pprint(json_new_list)
+
+print("************************************************************")
+print("********************ciscoconfparse**********************")
+print("************************************************************")
+
+Config_File = CiscoConfParse("lesson1_ConfigFile.txt")
+
+print("=== crypto map with pfs group2 =====")
+crypto_w_pfs_group2 = Config_File.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"set pfs group2")
+print(crypto_w_pfs_group2)
+
+for parent in crypto_w_pfs_group2:
+  print parent.text
+  for child in parent.children:
+    print child.text
+
+print("=== crypto map without aes =====")
+
+crypto_w_pfs_group2 = Config_File.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"set transform-set AES")
+
+for parent in crypto_w_pfs_group2: 
+  print parent.text
+  for child in parent.children:
+    print child.text
 
