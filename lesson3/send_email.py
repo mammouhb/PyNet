b5919d3a142a6a0a8b8c4ebe7c1abd46b185cd35
Applied Python user <mammouhb@gmail.com>
2018-07-08 14:28:15 -0700

    send_email

---
+++ lesson3/send_email.py
@@ -0,0 +1,44 @@
+def send_mail(recipient, subject, message, sender):
+    '''
+    Simple function to help simplify sending SMTP email
+
+    Assumes a mailserver is available on localhost
+    '''
+
+    import smtplib
+    from email.mime.text import MIMEText
+
+    message = MIMEText(message)
+    message['Subject'] = subject
+    message['From'] = sender
+    message['To'] = recipient
+
+    # Create SMTP connection object to localhost
+    smtp_conn = smtplib.SMTP('localhost')
+
+    # Send the email
+    smtp_conn.sendmail(sender, recipient, message.as_string())
+
+    # Close SMTP connection
+    smtp_conn.quit()
+    return True
+
+
+import email_helper
+
+recipient = 'mammouhb@gmail.com'
+subject = 'Test message'
+message = '''
+
+This is a fictional test message.
+
+
+Regards,
+
+Bouchaib
+
+'''
+
+sender = 'mammouhb@gmail.com'
+email_helper.send_mail(recipient, subject, message, sender) 
+
